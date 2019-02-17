576e0c7e36b77448dc6024afe7122dc3a5829673
kalidossbtech <kalidossbtech@gmail.com>
2019-02-17 20:22:01 +0530

    allignment over

--- dir_structure.py
+++ dir_structure.py
@@ -52,7 +52,7 @@ import os, sys
 
 pwd = os.getcwd()
 HOME_DIR = pwd
-PROJECT_NAME = sys.argv[2]
+PROJECT_NAME = 'project_name' ###sys.argv[2]
 TOP_DIR = ['config', 'docs', 'group_vars', 'host_vars', 'inventory', 'playbooks', 'plugins', 'roles']
 
 
@@ -67,39 +67,23 @@ class c_dir:
         ansi_file = open('ansible.cfg', 'w')
         ansi_file.write('#Write your Ansible configuration here')
         ansi_file.close()
-        vault_file = open('vault_password', 'w')
-        vault_file.close()
+        vault_file = open('vault_password', 'w');vault_file.close()
 
     def docs():
         pwd_docs = os.path.join(os.path.join(HOME_DIR, PROJECT_NAME), 'docs')
         os.chdir(pwd_docs)
-        ss = open('skel-start.md', 'w')
-        ss.write('')
-        ss.close()
-        sstips = open('skel-tips.md', 'w')
-        sstips.write('')
-        sstips.close()
+        ss = open('skel-start.md', 'w'); ss.close()
+        sstips = open('skel-tips.md', 'w'); sstips.close()
 
     def group_vars():
         pwd_group_vars = os.path.join(os.path.join(HOME_DIR, PROJECT_NAME), 'group_vars')
         os.chdir(pwd_group_vars)
 
-        g_all = open('all.yml', 'w')
-        g_all.write('')
-        g_all.close()
-
-        g_dev = open('dev.yml', 'w')
-        g_dev.write('')
-        g_dev.close()
-
+        g_all = open('all.yml', 'w'); g_all.close()
+        g_dev = open('dev.yml', 'w'); g_dev.close()
         g_link = os.symlink('dev.yml','default.ini')
-        g_preprod = open('preprod.yml', 'w')
-        g_preprod.write('')
-        g_preprod.close()
-
-        g_prod = open('prod.yml', 'w')
-        g_prod.write('')
-        g_prod.close()
+        g_preprod = open('preprod.yml', 'w'); g_preprod.close()
+        g_prod = open('prod.yml', 'w'); g_prod.close()
 
     def host_vars(): pass
 
@@ -107,26 +91,17 @@ class c_dir:
         pwd_inventory = os.path.join(os.path.join(HOME_DIR, PROJECT_NAME), 'inventory')
         os.chdir(pwd_inventory)
 
-        i_dev = open('dev.ini', 'w');i_dev.write('');i_dev.close()
-
-        i_preprod = open('preprod.ini', 'w')
-        i_preprod.write('')
-        i_preprod.close()
-
-        i_prod = open('preod.ini', 'w')
-        i_prod.write('')
-        i_prod.close()
+        i_dev = open('dev.ini', 'w');i_dev.close()
+        i_preprod = open('preprod.ini', 'w');i_preprod.close()
+        i_prod = open('preod.ini', 'w');i_prod.close()
 
     # symbolic link creation
 
     def playbooks():
         pwd_inventory = os.path.join(os.path.join(HOME_DIR, PROJECT_NAME), 'playbooks')
         os.chdir(pwd_inventory)
-        #i = str()
         for i in ['0_local_requirements.yaml', '1_target_test.yml', '2_target_requirements.yml', '3_target_sysadmin.yml']:
-            pb = open('%s'%i, 'w')
-            pb.write('')
-            pb.close()
+            pb = open('%s'%i, 'w');pb.close()
 
     def plugins():
         pwd_inventory = os.path.join(os.path.join(HOME_DIR, PROJECT_NAME), 'plugins')
@@ -135,15 +110,9 @@ class c_dir:
             os.mkdir('%s'%i)
 
     def roles():
-        pwd_inventory = os.path.join(os.path.join(HOME_DIR, PROJECT_NAME), 'roles')
-        os.chdir(pwd_inventory)
-        for i in ['local', 'profiles', 'vendors']:
-            os.mkdir('%s' % i)
-        req = open('requirements.yml', 'w')
-        req.write('')
-        req.close()
-
-
+        pwd_inventory = os.path.join(os.path.join(HOME_DIR, PROJECT_NAME), 'roles');os.chdir(pwd_inventory)
+        for i in ['local', 'profiles', 'vendors']: os.mkdir('%s' % i)
+        req = open('requirements.yml', 'w');req.close()
 
 
 print("We are going to create the directory structure of ansible in %s" % pwd)
@@ -162,44 +131,21 @@ if yn in ['yes', 'y', 'YES', 'Y', 'Yes']:
         # creating top level dir
         for top_dir in TOP_DIR:
             os.mkdir(top_dir)
-        # print('top_dir_created')
-
-
-
 
         # config dir creation
-
-        c_dir.config()
-        c_dir.docs()
-        c_dir.group_vars()
-        c_dir.inventory()
-        c_dir.playbooks()
-        c_dir.plugins()
-        c_dir.roles()
+        c_dir.config();c_dir.docs();c_dir.group_vars();c_dir.inventory();c_dir.playbooks();c_dir.plugins();c_dir.roles()
         #Creating outside files
         os.chdir(os.path.join(HOME_DIR, PROJECT_NAME))
 
-        print("Readme.txt file creation")
-        readme = open('Readme.txt', 'w')
-        readme.write('#Enter about your Project')
-        readme.close()
+        #Read me file creation
+        readme = open('Readme.txt', 'w');readme.write('#Enter about your Project');readme.close()
 
-        print("LICENSE file creation")
-        lic = open('LICENSE', 'w')
-        lic.write('#License details of your project')
-        lic.close()
-
-        print("Hosts file creation")
-        lic = open('hosts', 'w')
-        lic.write('')
-        lic.close()
-
-        print("Ansible file link")
+        #LICENSE file creation
+        lic = open('LICENSE', 'w');lic.write('#License details of your project');lic.close()
+        host = open('hosts', 'w');host.close()
         ansi_link = os.path.join(os.path.join(os.path.join(HOME_DIR, PROJECT_NAME), 'config'),'ansible.cfg')
         os.symlink(ansi_link,'ansible.cfg')
+        print('Project created Successfully')
 
-
-elif yn in ['no', 'n', 'NO', 'N', 'No']:
-    print('yn is on no')
-else:
-    print('Go to the Hell')
+elif yn in ['no', 'n', 'NO', 'N', 'No']: print('yn is on no')
+else: print('Go to the Hell')
